2496b7ba7dd58f4e120f6e6c5885e6018bbd21bc
Tony <tonymichelbe@gmail.com>
2024-04-08 13:59:35 +0200

    moved faq js to main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -259,4 +259,28 @@
 
 			});
 
+			// FAQ.
+			const faqItems = document.querySelectorAll('.faq-item');
+		
+			faqItems.forEach(item => {
+				const question = item.querySelector('.question');
+		
+				question.addEventListener('click', () => {
+					item.classList.toggle('expanded');
+					const answer = item.querySelector('.answer');
+					const arrowRight = question.querySelector('.arrow-right');
+					const arrowDown = question.querySelector('.arrow-down');
+		
+					if (item.classList.contains('expanded')) {
+						answer.style.display = 'block';
+						arrowRight.style.display = 'none';
+						arrowDown.style.display = 'inline-block';
+					} else {
+						answer.style.display = 'none';
+						arrowRight.style.display = 'inline-block';
+						arrowDown.style.display = 'none';
+					}
+				});
+			});
+
 })(jQuery);
